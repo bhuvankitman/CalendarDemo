54622d46096c46a3161db26d7acab8afc8ac51ec
bhuvankitman <bbhatt@kitmanlabs.com>
2023-09-27 16:40:49 +0100

    Clean monthly view model.

--- CalendarDemo/Calendar/Monthly/MonthlyViewModel.swift
+++ CalendarDemo/Calendar/Monthly/MonthlyViewModel.swift
@@ -8,54 +8,13 @@ struct Month {
 
 struct MonthlyViewModel {
 
-  // MARK: - Properties
-//  var nextIndex: Int {
-//    months.count > 0 ? months.count : 0
-//  }
-
   // MARK: - Initializer
   let selectedDate: Date
   let months: [Month]
 
-//  init(selectedDate: Date, monthBackwards: Int, monthForwards: Int) {
-//    self.selectedDate = selectedDate
-//
-//    // Past months
-//    months.append(contentsOf: getBackwardMonths(fromDate: selectedDate,
-//                                                monthBackwards: monthBackwards))
-//
-//    // Month for the given date
-//    months.append(.init(id: nextIndex,
-//                        title: selectedDate.monthString,
-//                        days: getMonthDays(forDate: selectedDate)))
-//
-//    // Future months
-//    months.append(contentsOf: getForwardMonths(fromDate: selectedDate,
-//                                               monthForwards: monthForwards,
-//                                               nextIndex: nextIndex))
-//  }
-
   init(selectedDate: Date, months: [Month]) {
     self.selectedDate = selectedDate
     self.months = months
-  }
-
-  func getBackwardMonths(fromDate date: Date, monthBackwards: Int) -> [Month] {
-    return Array(0..<monthBackwards).map { count in
-      let pastMonthDate = date.minusMonth(monthBackwards - count)
-      return Month(id: count,
-                   title: pastMonthDate.monthString,
-                   days: getMonthDays(forDate: pastMonthDate))
-    }
-  }
-
-  func getForwardMonths(fromDate date: Date, monthForwards: Int, nextIndex: Int) -> [Month] {
-    return Array(0..<monthForwards).map { count in
-      let futureMonthDate = date.plusMonth(count + 1)
-      return Month(id: nextIndex + count,
-                   title: futureMonthDate.monthString,
-                   days: getMonthDays(forDate: futureMonthDate))
-    }
   }
 }
 
@@ -73,29 +32,4 @@ extension MonthlyViewModel {
     month(for: section)?.days ?? []    
   }
 
-  func getMonthDays(forDate date: Date) -> [Day] {
-
-    var days = [Day]()
-
-    let daysInMonth = date.daysInMonth
-    let firstDayOfMonth = date.firstOfMonth
-    let startingSpaces = firstDayOfMonth.weekDay
-
-    var count: Int = 1
-
-    let monthYear = date.monthString + " " + date.yearString
-
-    while(count <= 42) {
-      if count <= startingSpaces || count - startingSpaces > daysInMonth {
-        days.append(.init(day: ""))
-      } else {
-        let day = String(count - startingSpaces)
-        let strDate = day + " " + monthYear
-        days.append(.init(day: day, date: strDate.date))
-      }
-      count += 1
-    }
-
-    return days
-  }
 }
